1bf1672b41ba0308e268cea322e63a28ea537ab0
BlakeMar <blake.martin@drake.edu>
2015-09-23 20:25:23 -0500

    Implemented search bar function

--- app/src/main/java/com/projectone/cs188/projectone/EventScreen.java
+++ app/src/main/java/com/projectone/cs188/projectone/EventScreen.java
@@ -1,10 +1,12 @@
 package com.projectone.cs188.projectone;
 
+import android.content.Intent;
 import android.graphics.Typeface;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 import android.widget.TextView;
 
 public class EventScreen extends AppCompatActivity {
@@ -14,6 +16,11 @@ public class EventScreen extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_event_screen);
 
+    }
+
+    public void toSearchScreen(View view) {
+        Intent intent = new Intent(this, SearchScreen.class);
+        startActivity(intent);
     }
 
     @Override
